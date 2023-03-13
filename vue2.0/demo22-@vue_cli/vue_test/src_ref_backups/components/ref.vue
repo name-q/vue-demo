9fcadae0e7c28747ce47e26e997ee4a9c6d21060
name-q <1620206666@qq.com>
2023-03-13 14:53:46 +0800

    ref backups

---
+++ vue2.0/demo22-@vue_cli/vue_test/src_ref_backups/components/ref.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello" @click.stop="handleClick">
+    <h1 v-text="msg" ref="title"></h1>
+    <RefChild ref="child" />
+  </div>
+</template>
+
+<script>
+import RefChild from "./refchild.vue";
+
+export default {
+  name: "RefView",
+  props: {
+    msg: String,
+  },
+  components: {
+    RefChild,
+  },
+  methods: {
+    handleClick() {
+      console.log("获取到自身的ref", this.$refs.title);
+      console.log("获取到自组件的ref", this.$refs.child, "为子组件的VC");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: antiquewhite;
+}
+</style>
---
+++ vue2.0/demo22-@vue_cli/vue_test/src_ref_backups/components/refchild.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello" @click.stop="handleClick">
+    <h1 v-text="msg" ref="info"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RefChildView",
+  methods: {
+    handleClick() {
+      console.log(1);
+    },
+  },
+  data: function () {
+    return {
+      msg: "hello ref child",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: aqua;
+}
+</style>
