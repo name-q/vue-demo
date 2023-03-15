bf0dedf7428c784451fab574ce6af91bde9528ce
name-q <1620206666@qq.com>
2023-03-15 14:57:40 +0800

    组件解绑自定义事件 props function

--- vue2.0/demo22-@vue_cli/vue_test/src/components/refchild.vue
+++ vue2.0/demo22-@vue_cli/vue_test/src/components/refchild.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1 v-text="msg" ref="info"></h1>
     <button @click.stop="handleClickChild">propsFunction</button>
+    <button @click.stop="unbind">unbind propsFunction</button>
   </div>
 </template>
 
@@ -19,6 +20,16 @@ export default {
   methods: {
     handleClickChild() {
       this.$emit("propsFunction", Date.now());
+    },
+    unbind() {
+      // 解绑props function
+      this.$off("propsFunction");
+      // 解绑多个 props function
+      // this.$off(["fun1", "fun2"]);
+      // 解绑所有 props function
+      // this.$off();
+      // 销毁这个组件导致所有 props function 被销毁 此时组件依旧显示但click事件亦不存在造成穿透点击
+      // this.$destroy()
     },
   },
 };
