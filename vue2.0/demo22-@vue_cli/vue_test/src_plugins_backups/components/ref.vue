7ee96a86f5299e9893028917536d7c7182e69e97
name-q <1620206666@qq.com>
2023-03-15 13:50:43 +0800

    plugins backups

---
+++ vue2.0/demo22-@vue_cli/vue_test/src_plugins_backups/components/ref.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello" @click.stop="handleClick">
+    <h1 v-text="msg" ref="title"></h1>
+    <h2 v-text="msgx" ref="title"></h2>
+    <RefChild ref="child" />
+  </div>
+</template>
+
+<script>
+import RefChild from "./refchild.vue";
+import { getNow } from "./mixin";
+
+export default {
+  name: "RefView",
+  // props: ['msg', 'msgx'],
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+    msgx: {
+      type: String,
+      default: "hello props",
+    },
+  },
+  components: {
+    RefChild,
+  },
+  mixins: [getNow],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: antiquewhite;
+}
+</style>
---
+++ vue2.0/demo22-@vue_cli/vue_test/src_plugins_backups/components/refchild.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello" @click.stop="mixinHandleClick">
+    <h1 v-text="msg" ref="info"></h1>
+    <h2>{{ info | getYYYY }}</h2>
+    <button @click.stop="handleclick">click Vue.prototype.hello</button>
+  </div>
+</template>
+
+<script>
+import { getNow } from "./mixin";
+
+export default {
+  name: "RefChildView",
+  data() {
+    return {
+      msg: "hello ref child",
+      info: "abcdefghijklmn",
+    };
+  },
+  methods: {
+    handleclick() {
+      this.hello();
+    },
+  },
+  mixins: [getNow],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: aqua;
+}
+</style>
