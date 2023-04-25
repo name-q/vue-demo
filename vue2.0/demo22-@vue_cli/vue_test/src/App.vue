2bd598aac24686f39bd5c334889849e68b7c0202
name-q <1620206666@qq.com>
2023-04-25 15:24:08 +0800

    axios获取数据 代理解决CORS

--- vue2.0/demo22-@vue_cli/vue_test/src/App.vue
+++ vue2.0/demo22-@vue_cli/vue_test/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    
+    <button @click="getstudents">获取学生信息</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'App',
+  methods: {
+    async getstudents() {
+      try {
+        let result = await axios.get('http://localhost:8080/students')
+        console.log(result, '<<<')
+      } catch (error) {
+        console.log(error, '<<<err')
+      }
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
