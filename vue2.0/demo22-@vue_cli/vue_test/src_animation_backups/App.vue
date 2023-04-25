ec618934e6ec5d194fffe3d9bd7dcd3bdd021541
name-q <1620206666@qq.com>
2023-04-25 15:11:45 +0800

    animation backups

--- vue2.0/demo22-@vue_cli/vue_test/src_animation_backups/App.vue
+++ vue2.0/demo22-@vue_cli/vue_test/src_animation_backups/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Test/>
   </div>
 </template>
 
 <script>
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
+  components: {
+    Test
+  }
 }
 </script>
 
---
+++ vue2.0/demo22-@vue_cli/vue_test/src_animation_backups/components/Test.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition>
+      <h1 v-show="isShow">animation</h1>
+    </transition>
+    <transition name="xxx" appear>
+      <h1 v-show="isShow" class="go">animation name</h1>
+    </transition>
+
+    <transition appear name="animate__animated animate__bounce" enter-active-class="animate__swing"
+      leave-active-class="animate__backOutUp">
+      <h1 v-show="isShow">animation animate.css</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+import 'animate.css';
+
+export default {
+  name: "Test",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+
+/* 
+.come {
+  animation: come .3s ease-in;
+}
+
+.go {
+  animation: come .3s reverse;
+} 
+*/
+
+.v-enter-active {
+  animation: come 0.3s ease-in;
+}
+
+.v-leave-active {
+  animation: come 0.3s linear reverse;
+}
+
+/*
+xxx-enter            进入前效果
+xxx-enter-active     进入的过渡时间和函数
+xxx-enter-to         进入后效果
+xxx-leave            离开前效果
+xxx-leave-active     离开的过渡时间和函数
+xxx-leave-to         离开后效果
+*/
+.xxx-enter-active {
+  animation: come 0.3s ease-in;
+}
+
+.xxx-leave-active {
+  animation: come 0.3s linear reverse;
+}
+
+@keyframes come {
+  from {
+    transform: translateX(-100%);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+}
+</style>
